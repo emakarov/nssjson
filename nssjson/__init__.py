cad78eab1b71d046933edf7a0e9f11f4bf1d88e0
Lele Gaifax <lele@metapensiero.it>
2015-08-07 23:42:49 +0200

    Tweak examples in the module documentation to avoid ambiguity

--- nssjson/__init__.py
+++ nssjson/__init__.py
@@ -18,12 +18,12 @@ Encoding basic Python object hierarchies::
     >>> import nssjson as json
     >>> json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
     '["foo", {"bar": ["baz", null, 1.0, 2]}]'
-    >>> print(json.dumps("\"foo\bar"))
-    "\"foo\bar"
-    >>> print(json.dumps(u'\u1234'))
-    "\u1234"
-    >>> print(json.dumps('\\'))
-    "\\"
+    >>> json.dumps('"foo\bar')
+    '"\\"foo\\bar"'
+    >>> print(repr(json.dumps(u'\u1234')))
+    '"\\u1234"'
+    >>> json.dumps('\\\\')
+    '"\\\\\\\\"'
     >>> print(json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True))
     {"a": 0, "b": 0, "c": 0}
     >>> from nssjson.compat import StringIO
